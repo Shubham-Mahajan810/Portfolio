d8fae25ce499ce5bd83ffd07705ea78674b4ce0f
shubham Mahajan <mahajanshubham8100@gmail.com>
2025-07-21 00:09:49 +0530

    Update the new resume

--- Resume.js
+++ Resume.js
@@ -1,7 +1,7 @@
 document.getElementById('Resume').addEventListener('click', function (event) {
     event.preventDefault(); // Prevent the default behavior of the anchor link
     const link = document.createElement('a');
-    link.href = './assets/Shubham Mahajan.pdf'; // Use the correct relative or absolute path
+    link.href = './assets/ShubhamMahajan_Resume.pdf'; // Use the correct relative or absolute path
     link.download = 'Shubham Mahajan.pdf'; // Set the download file name
     document.body.appendChild(link); // Temporarily add the link to the document
     link.click(); // Trigger the download
@@ -9,4 +9,3 @@ document.getElementById('Resume').addEventListener('click', function (event) {
 }); 
 
 
-  
